d0b5a7a51b407fcc7f46fae68a8735336ba04aa8
Ibimina hart <ibimina.c.hart@gmail.com>
2023-03-22 00:45:20 +0100

    docs: basic routing

--- steptwo_view-engines/app.js
+++ steptwo_view-engines/app.js
@@ -1,4 +1,4 @@
-const express = reqiure("express")
+const express = require("express")
 
 const app = express()
 
@@ -6,4 +6,24 @@ app.set('view engine',"ejs")
 
 app.listen(3000)
 
+app.get('/',(req,res)=>{
+res.render('index')
+})
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/about-us", (req, res) => {
+  res.redirect/("about");
+});
+
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+//404
+app.use((req, res) => {
+  res.status(404).render("404");
+});
